13671a9f55e569e43edff7a8583558d838b0bcb2
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2022-09-08 18:03:08 +0530

    Update div.js

--- div.js
+++ div.js
@@ -65,7 +65,7 @@ const generateFloors = (n)=> {
     document.getElementById('simulationArea').innerHTML = ''
     for (let i=0;i<n;i++) {
         let currLevel = `L${n-i-1}`
-        let floorNo = `${n - i - 1}`
+        let floorNo = `Floor${n - i - 1}`
         let currFloor = document.createElement('div')
         currFloor.setAttribute('id',floorNo)
 
