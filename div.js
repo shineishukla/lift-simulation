614eb809a6ee9aa8b44ee2d627a02ee15cfadf0d
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2022-10-10 16:17:30 +0530

    Update div.js

--- div.js
+++ div.js
@@ -87,7 +87,7 @@ const generateFloors = (n)=> {
         currFloor.innerHTML = `
         <p>${floorNo}</p>
         <div>
-        <button id=up${currLevel} class="button-floor upBttn">Up</button><br>
+        <button id=up${currLevel} class="button-floor upBttn">Up</button>
         <button id=down${currLevel} class="button-floor downBttn">Down</button>
         </div>
         `;
